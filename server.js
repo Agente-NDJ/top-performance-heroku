e849cb28778089d8464cefb79cd77921fd20e8d7
Agente-NDJ <94809985+Agente-NDJ@users.noreply.github.com>
2023-12-21 10:48:05 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Define a route that sends a JSON response
+ 
+const jsonData = [
+  {
+    certificationName: 'Salesforce Advanced Course',
+    domain: 'Salesforce',
+    rewardValue: '300€'
+  },
+  {
+    certificationName: 'Apex Triggers Beginner',
+    domain: 'Apex',
+    rewardValue: '100€'
+  },
+  {
+    certificationName: 'MS Dynamic Advanced Course',
+    domain: 'MS Dynamics',
+    rewardValue: '200€'
+  },
+  {
+    certificationName: 'Mulesoft Advanced API Course',
+    domain: 'Mulesoft',
+    rewardValue: '200€'
+  },
+  // Add more data as needed
+];
+ 
 app.get('/api/data', (req, res) => {
-  const jsonData = {
-    message: 'Hello, this is your JSON response!',
-    timestamp: new Date()
-  };
-
   res.json(jsonData);
 });
-
-// Start the server
+ 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
